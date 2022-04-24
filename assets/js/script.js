2f84be618804a29b7ed26ce92a7e7465d1161d6d
CMiller <mlr.chase40@gmail.com>
2022-04-24 17:20:47 -0500

    UX enhancements and updated README

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,7 +185,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -223,19 +223,19 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     $(this).addClass("dropover");
-    $(".bottom-trash").addClass(".bottom-trash-drag");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     $(this).removeClass("dropover");
-    $(".bottom-trash").removeClass(".bottom-trash-drag");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     $(this).addClass("dropover-active");
-    $(".bottom-trash").addClass(".bottom-trash-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     $(this).removeClass("dropover-active");
-    $(".bottom-trash").removeClass(".bottom-trash-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   update: function(event) {
     var tempArr = [];
